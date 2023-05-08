3d7e799b45b38cdc1dbad541779732c5f0c68cb4
TNMHuy <mhuy2898@gmail.com>
2023-05-08 19:26:37 +0700

    update

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+
+const getEle = (id)=>{
+    return document.getElementById(id)
+}
+let index = 0
+
+
+const next = () => {
+  if(index<2){
+    index++
+    getEle('slideTrans').style.transform = `translateX(-${index * 1360}px)`;
+    getEle('slideTrans').style.transition="transform 0.5s"
+  }else{
+    index=0
+    getEle('slideTrans').style.transform = `translateX(0)`;
+  
+
+  }
+}
+const prev = () => {
+  if(index>0){
+    index--
+    getEle('slideTrans').style.transform = `translateX(-${index * 1360}px)`;
+    getEle('slideTrans').style.transition="transform 0.5s"
+  }else{
+    index=2
+    getEle('slideTrans').style.transform = `translateX(-${index * 1360}px)`
+    
+  }
+}
+const autoTrans =() => {
+  if(index<2){
+    index++
+    getEle('slideTrans').style.transform = `translateX(-${index * 1360}px)`;
+    getEle('slideTrans').style.transition="transform 0.5s"
+  }else{
+    index=0
+    getEle('slideTrans').style.transform = `translateX(0)`;
+  }
+
+}
+
+  setInterval('autoTrans()',5000)  
+// nextBtn.addEventListener("click", next);
+// prevBtn.addEventListener("click", prev);
+
+
